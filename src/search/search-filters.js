e1a9ac44cb2637312697ac8adc06c9a068539c1c
Arash Rowshan <arash.rowshan@gmail.com>
2017-10-14 21:45:45 -0400

    renders filters in ul li format

--- src/search/search-filters.js
+++ src/search/search-filters.js
@@ -3,47 +3,33 @@ import React, { Component } from 'react';
 class SearchFilters extends Component {
 
   listFilters(filters) {
-    filters.forEach( (filter) => {
+    return filters.map( (filter) => {
       if(typeof(filter)==='string' || typeof(filter)==='number') {
-        // return (
-        //   <li key={filter}>
-        //     <input type="checkbox"/>{filter}
-        //   </li> 
-        // );
-        console.log(filter);
+        return (
+          <li key={filter}>
+            <input type="checkbox"/>{filter}
+          </li> 
+        );
       }
       else if(typeof(filter==='object')) {
-        if(Array.isArray(filter)) {
-          this.listFilters(filter);
-          console.log('AAAAAAAAAAAAAAAAAAAAAA');
-        } else {
-          this.listFilters(filter[Object.keys(filter)[0]]);
-        }
+        return (
+          <li><input type="checkbox"/>{Object.keys(filter)[0]}
+            <ul>
+              {this.listFilters(filter[Object.keys(filter)[0]])}
+            </ul>
+          </li>
+        )
       }
-    });
-  }
-  
-  renderFilters() {
-    return this.props.searchFilters.map(filter => {
-      return (
-        <li key={filter}>
-          <input type="checkbox"/>
-          {filter}
-        </li>
-      );
     });
   }
 
   render() {
-    // console.log(this.props.searchFilters);
-    this.listFilters(this.props.searchFilters);
-    // return (
-    //   <ul>
-    //     {this.renderFilters()}
-    //   </ul>
-    // );
     return (
-      'as'
+      <div>
+        <ul>
+          {this.listFilters(this.props.searchFilters)}
+        </ul>
+      </div>
     );
   }
 
